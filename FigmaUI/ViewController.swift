87894dcb2ab90851885fd7a098345007cd220802
Ashin <ashinwangboy@gmail.com>
2021-11-27 23:24:51 +0800

    Initial Commit

---
+++ FigmaUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FigmaUI
+//
+//  Created by Ashin Wang on 2021/11/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
